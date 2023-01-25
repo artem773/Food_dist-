abd152d81aca84142161e3a069a19755ba930a99
artem <example@gmail.com>
2023-01-25 12:48:36 +0200

    first push

---
+++ js/script.js
@@ -0,0 +1,325 @@
+window.addEventListener('DOMContentLoaded', ()=>{
+
+    ///////////
+    /* TABS */
+    //////////
+    
+    let tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+    
+    function hideTabContent() {
+
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item =>{
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) { // 0 - по умолчанію ставітся первий обьект у список ES6
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) =>{
+        const target = event.target;
+
+        if(target && target.classList.contains('tabheader__item')){
+            tabs.forEach((item, i) =>{
+                if (target == item){
+                    hideTabContent();
+                    showTabContent(i);  
+                }
+            });
+        }
+    });
+
+    ///////////
+    /* TIMER */
+    ///////////
+
+    const deadLine = '2023-02-28'; //нужно что би дата била позже фактіческой (как в реальном дедлайне)
+
+    function getTimeRemainig(endtime){
+
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date()); //разніца между дедлайном і текущей датой (в міллісекундах)
+
+        if(t <= 0){
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        }else{
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),  //дні в міллісек
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)), //часи в міллісек....
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        }
+              
+        return{
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds,
+            };
+    }
+
+    function getZero(num){
+        if(num >= 0 && num < 10){
+            return `0${num}`;
+        }else{
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000); //обновляет таймер каждую секунду
+
+        updateClock();
+
+        function updateClock(){
+            const t = getTimeRemainig(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if(t.total <= 0){
+                clearInterval(timeInterval);
+            }
+        }   
+    } 
+
+    setClock('.timer', deadLine);
+
+    ///////////
+    /* Modal */
+    ///////////
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal');
+
+    function openModal(){
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden'; //убірает прокрутку когда окно открито
+        clearInterval(modalTimerId);
+    }
+    
+    modalTrigger.forEach(btn=>{
+        btn.addEventListener('click', ()=>{
+             openModal();
+        });
+    });
+    
+
+    function closeModal(){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = ''; //добавляет прокрутку когда окно закрито
+    }
+
+    
+
+    modal.addEventListener('click', (e)=>{
+        if(e.target === modal || e.target.getAttribute('data-close') == ''){
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e)=>{
+        if(e.code === 'Escape' && modal.classList.contains('show')){
+            closeModal();
+        }
+    });
+
+    
+    const modalTimerId = setTimeout(openModal, 50000); //откривает модальное окно чз 5 сек после того как юзер зашел на сайт 
+
+    function showModalByScroll(){
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1){ //окно будет появляться не доходя до последнего пікселя монітора(-1);
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
+    ///////////////////////////////////
+    /* Используем класи для корточек */
+    ///////////////////////////////////
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH(); 
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer; 
+        }
+
+        render() {
+            const element = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.classes = "menu__item";
+                element.classList.add(this.classes);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container"
+    ).render();
+
+    ////////////
+    /* Forms */
+    ///////////
+
+    //что б бекенд работал нужно чістіть кеш после каждоо ізмененія в коде CMD + SHIFT + R
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: './img/form/spinner.svg',
+        succes: 'Спасибо! Скоро ми с вами свяжемся',
+        failure: 'Что-то пошло не так'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    });
+
+    function postData(form){
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault(); // убіраем стандартное поведеніе браузера (что би он не перезагружался)
+
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText =`
+                display: block;
+                margin: 0 auto;
+            `;
+
+            form.insertAdjacentElement('afterbegin', statusMessage);
+
+            const formData = new FormData(form);
+
+            const object = {};
+            formData.forEach(function(key, value){
+                object[key] = value;
+            });
+
+            //отправка на сервер json формата с помощью метода fetch
+            fetch('./server/server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.succes);
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finnaly(() => {
+                form.reset();
+            });
+        });
+    }
+
+    // функція что создает новое окно что сигналізірует о том что данние отправлени на сервер
+    function showThanksModal(message){
+        const prevModalDialg = document.querySelector('.modal__dialog');
+
+        prevModalDialg.classList.add('hide');
+        openModal(); // откривает модальное окно когда функція визивается 
+
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+
+            <div class="modal__content">
+                <div class="modal__close" data-close>&times;</div>
+                <div class="modal__title">${message}</div>
+            </div>
+        `;
+
+        document.querySelector('.modal').append(thanksModal);
+
+        setTimeout(()=>{
+            thanksModal.remove();
+            prevModalDialg.classList.add('show');
+            prevModalDialg.classList.remove('hide');
+            closeModal();
+        }, 4000);
+    }
+
+
+}); 
